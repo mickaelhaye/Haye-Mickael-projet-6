ebb9aee285e6b3d724a50ebb4c847e6668a81026
Mickael Haye <mickael.haye@gmail.com>
2023-06-22 10:11:51 +0200

    Correction @OnetoMany n'était pas dans le bonne classe

--- src/main/java/com/paymybuddy/model/UserModel.java
+++ src/main/java/com/paymybuddy/model/UserModel.java
@@ -1,14 +1,21 @@
 package com.paymybuddy.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.hibernate.annotations.DynamicUpdate;
 
 import com.paymybuddy.model.abstractmodel.PersonModel;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -38,4 +45,15 @@ public class UserModel extends PersonModel {
 	@Column(name = "password")
 	private String password;
 
+	@OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+	@JoinColumn(name = "user_id")
+	private List<ExternCountModel> externCounts = new ArrayList<>();
+
+	@OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+	@JoinColumn(name = "user_id")
+	private List<FriendModel> friends = new ArrayList<>();
+
+	@OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+	@JoinColumn(name = "user_id")
+	private List<CountModel> counts = new ArrayList<>();
 }
