2b582e25c53d624f3e62f7a4fc6c282fa59dbf98
Mickael Haye <mickael.haye@gmail.com>
2023-06-27 13:30:42 +0200

    Implementation methode CRUD dans controller avec API

--- src/main/java/com/paymybuddy/controller/AccountController.java
+++ src/main/java/com/paymybuddy/controller/AccountController.java
@@ -1,8 +1,40 @@
 package com.paymybuddy.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.paymybuddy.model.entity.AccountModel;
+import com.paymybuddy.service.AccountService;
 
 @RestController
 public class AccountController {
+
+	@Autowired
+	private AccountService accountService;
+
+	@GetMapping("/account")
+	public Iterable<AccountModel> displayAccountList() {
+		return accountService.getAccounts();
+	}
+
+	@PostMapping("/account")
+	public void addAccount(@RequestBody AccountModel account) {
+		accountService.addAccount(account);
+	}
+
+	@PatchMapping("/account")
+	public void updateAccount(@RequestBody AccountModel account) {
+		accountService.addAccount(account);
+	}
+
+	@DeleteMapping("/account")
+	public void deleteAccount(@RequestBody AccountModel account) {
+		accountService.delAccount(account);
+	}
 
 }
