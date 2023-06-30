f61bd70edaa625952683fb2eb51a70d8c22a98e7
Mickael Haye <mickael.haye@gmail.com>
2023-06-30 11:11:06 +0200

    création de la classe UserController sans @RestController

--- src/main/java/com/paymybuddy/controller/UserController.java
+++ src/main/java/com/paymybuddy/controller/UserController.java
@@ -1,39 +1,40 @@
 package com.paymybuddy.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.paymybuddy.model.entity.UserModel;
 import com.paymybuddy.service.UserService;
 
-@RestController
+@Controller
 public class UserController {
+
 	@Autowired
 	private UserService userService;
 
-	@GetMapping("/user")
-	public Iterable<UserModel> displayUserList() {
-		return userService.getUsers();
+	@GetMapping("/paymybuddy")
+	public String start() {
+		return "paymybuddy";
 	}
 
-	@PostMapping("/user")
-	public void addUser(@RequestBody UserModel user) {
+	@GetMapping("/users/new")
+	public String createUserForm(Model model) {
+
+		// create user object to hold user form data
+		UserModel user = new UserModel();
+		model.addAttribute("user", user);
+		return "create_user";
+	}
+
+	@PostMapping("/users")
+	public String saveUser(@ModelAttribute("user") UserModel user) {
 		userService.addUser(user);
-	}
 
-	@PatchMapping("/user")
-	public void updateUser(@RequestBody UserModel user) {
-		userService.addUser(user);
-	}
+		return "redirect:/paymybuddy";
 
-	@DeleteMapping("/user")
-	public void deletePerson(@RequestBody UserModel user) {
-		userService.delUser(user);
 	}
-
 }
